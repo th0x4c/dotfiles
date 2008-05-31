b72068718340ce09c8a1ef7a7e1adc522b090d1b
hashi <hashi@b51d36c4-7d92-49ba-b7cf-209faa32988c>
2008-05-31 15:06:08 +0000

    Modify init-xcscope.el to run xcscope on objc-mode.

--- .emacs.d/init/init-xcscope.el
+++ .emacs.d/init/init-xcscope.el
@@ -2,3 +2,7 @@
 (require 'xcscope)
 ;; never check and/or update the cscope database when searching
 (setq cscope-do-not-update-database t)
+
+;; objc-mode で xcscope を起動
+(add-hook 'objc-mode-hook (function cscope:hook))
+
