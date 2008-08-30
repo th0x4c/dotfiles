bb51bb1b66d4db8869f0a9f4ded4f181a045d0b4
hashi <hashi@b51d36c4-7d92-49ba-b7cf-209faa32988c>
2008-08-30 11:08:42 +0000

    Modify init-xcscope.el. Change foreground color for line to be visible on a black background.

--- .emacs.d/init/init-xcscope.el
+++ .emacs.d/init/init-xcscope.el
@@ -6,3 +6,31 @@
 ;; objc-mode で xcscope を起動
 (add-hook 'objc-mode-hook (function cscope:hook))
 
+;; 2. By default, colored faces are used to display results.  If you happen
+;;    to use a black background, part of the results may be invisible
+;;    (because the foreground color may be black, too).  There are at least
+;;    two solutions for this:
+;;
+;;    2a. Turn off colored faces, by setting `cscope-use-face' to `nil',
+;;        e.g.:
+;;
+;;            (setq cscope-use-face nil)
+;;
+;;    2b. Explicitly set colors for the faces used by cscope.  The faces
+;;        are:
+;;
+;;            cscope-file-face
+;;            cscope-function-face
+;;            cscope-line-number-face
+;;            cscope-line-face
+;;            cscope-mouse-face
+;;
+;;        The face most likely to cause problems (e.g., black-on-black
+;;        color) is `cscope-line-face'.
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(cscope-line-face ((((class color) (background light)) (:foreground "white")))))
+
