1d0caccd31f2441090e6610d4f25b453a361960c
hashi <hashi@b51d36c4-7d92-49ba-b7cf-209faa32988c>
2010-09-05 15:52:30 +0000

    Use jdb instead of JDEbug in JDEE.

--- .emacs.d/init/init-jdee.el
+++ .emacs.d/init/init-jdee.el
@@ -3,16 +3,3 @@
 (add-to-list 'load-path "~/.emacs.d/elisp/elib")
 
 (require 'jde)
-
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(jde-debugger (quote ("JDEbug"))))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(cscope-line-face ((((class color) (background light)) (:foreground "white")))))
