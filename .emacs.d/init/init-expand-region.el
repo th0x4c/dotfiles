10ddde68e8b74d15e38395718cb2400741007315
Takashi Hashizume <th0x4c@gmail.com>
2018-04-08 02:26:17 +0900

    Change keybinding for expand-region due to C-SPC mapping to C-@ when emacs runs in the terminal.

--- .emacs.d/init/init-expand-region.el
+++ .emacs.d/init/init-expand-region.el
@@ -1,5 +1,4 @@
 ;; expand-region
 (require 'expand-region)
 
-(global-set-key (kbd "C-@") 'er/expand-region)
-(global-set-key (kbd "C-M-@") 'er/contract-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
