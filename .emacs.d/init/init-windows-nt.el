25158a034963c6d0482f61fe8c1158b6f5c1c09c
Takashi Hashizume <th0x4c@gmail.com>
2021-01-09 01:05:23 +0900

    Set the default font for Windows.

--- .emacs.d/init/init-windows-nt.el
+++ .emacs.d/init/init-windows-nt.el
@@ -2,3 +2,7 @@
 
 (server-start)
 (global-set-key [M-kanji] 'ignore)
+
+;; default font
+(custom-set-faces
+ '(default ((t (:family #("ＭＳ ゴシック" 0 7 (charset cp932-2-byte)) :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
