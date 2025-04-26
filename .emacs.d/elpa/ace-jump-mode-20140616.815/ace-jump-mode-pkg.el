150254e142d0306c06a714c34bac26143c582402
Takashi Hashizume <th0x4c@gmail.com>
2025-04-26 22:41:56 +0900

    Upgrade packages

---
+++ .emacs.d/elpa/ace-jump-mode-20140616.815/ace-jump-mode-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ace-jump-mode" "20140616.815"
+  "A quick cursor location minor mode for emacs."
+  ()
+  :url "https://github.com/winterTTr/ace-jump-mode/"
+  :commit "8351e2df4fbbeb2a4003f2fb39f46d33803f3dac"
+  :revdesc "8351e2df4fbb"
+  :keywords '("motion" "location" "cursor")
+  :authors '(("winterTTr" . "winterTTr@gmail.com"))
+  :maintainers '(("winterTTr" . "winterTTr@gmail.com")))
