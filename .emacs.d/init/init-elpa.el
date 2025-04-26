70ab214bc914ed1a55b035749dec665972819117
Takashi Hashizume <th0x4c@gmail.com>
2025-04-26 21:31:50 +0900

    Remove Marmalade repository that has been down

--- .emacs.d/init/init-elpa.el
+++ .emacs.d/init/init-elpa.el
@@ -6,7 +6,5 @@
              '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("marmalade" . "https://marmalade-repo.org/packages/"))
 
 (package-initialize)
